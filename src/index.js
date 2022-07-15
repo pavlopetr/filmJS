0b570ae73006d2ceed6acaccda728096cda731dd
lmc-cmd <63010584+lmc-cmd@users.noreply.github.com>
2022-07-15 23:28:05 +0300

    updLS

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { GalleryApi } from './js/gallery';
 import Notiflix from 'notiflix';
 import createCardOfMovie from './templates/filmCards.hbs';
 import createModal from './templates/modal.hbs';
+import './js/to_LocalStor';
 
 const formEl = document.querySelector('#search-form');
 const inputEl = document.querySelector('.imput_form');
