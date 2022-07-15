47fc7ce616a6d1540d6a4b17f1d6c0dda0ef4e56
lmc-cmd <63010584+lmc-cmd@users.noreply.github.com>
2022-07-16 00:38:49 +0300

    some localupd

--- src/js/to_LocalStor.js
+++ src/js/to_LocalStor.js
@@ -1,28 +1,28 @@
 'use strict';
 import ls from './storage';
 
-const containerEl = document.querySelector('.cards-film_list');
+const modalContainerEl = document.querySelector('.modal');
 
 let toWatchArry = [];
 let temp = ls.load(`toWatch`);
 console.dir(temp);
-if (toWatchArry.length === 0) {
-  toWatchArry = ls.load(`toWatch`);
-}
+// if (toWatchArry.length === 0) {
+//   toWatchArry = ls.load(`toWatch`);
+// }
 
 // console.log('before :>> ', toWatchArry);
 
-containerEl.addEventListener('click', e => {
+modalContainerEl.addEventListener('click', e => {
   e.preventDefault;
-  const parentLi = e.target.closest('li');
-  const filmIdEl = parentLi.querySelector(`.film-id`);
-  const filmId = filmIdEl.textContent.trim(` `);
-  // console.log('in :>> ', toWatchArry);
-
-  if (e.target.nodeName === 'IMG') {
-    toWatchArry.push(filmId);
-    localStorage.clear(`toWatch`);
+  const parentLi = modalContainerEl.querySelector('.film-id');
+  const filmID = parentLi.textContent;
+  // console.dir(e.target);
+  if (e.target.textContent === 'Add to watched') {
+    toWatchArry.push(filmID);
+    // localStorage.clear(`toWatch`);
     ls.save(`toWatch`, toWatchArry);
   }
 });
+
 // console.log('arrr :>> ', toWatchArry);
+// ['data-watched'].name;
