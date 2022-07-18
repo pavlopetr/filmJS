56bec01fc4948fdea39431705c4278b0686764e5
lmc-cmd <63010584+lmc-cmd@users.noreply.github.com>
2022-07-18 11:26:00 +0300

    sa

--- src/js/lightSwitch.js
+++ src/js/lightSwitch.js
@@ -11,13 +11,13 @@ if (switherMode === 'off') {
   cneckBoxEl.checked = true;
   bodyEl.classList.add('night');
   footerEl.classList.add('night');
-  // let Arry = Array(tittleEls);
-  // Array.prototype.forEach(tittleEls, function (tittleEls) {
-  //   return tittleEls.classList.add('night');
-  // });
-  // console.dir(Arry);
 }
 
+for (let el of tittleEls) {
+  if (switherMode === 'off') {
+    el = el.classList.add('nigthText');
+  }
+}
 cneckBoxEl.addEventListener('change', nigthlight);
 
 function nigthlight() {
@@ -25,17 +25,15 @@ function nigthlight() {
     ls.save('light', 'off');
     bodyEl.classList.add('night');
     footerEl.classList.add('night');
+    for (let el of tittleEls) {
+      el = el.classList.add('nigthText');
+    }
   } else {
     bodyEl.classList.remove('night');
     footerEl.classList.remove('night');
     ls.save('light', 'on');
+    for (let el of tittleEls) {
+      el = el.classList.remove('nigthText');
+    }
   }
 }
-// let elem = document.getElementById('t');
-// t.onclick = function () {
-//   if (this.style.backgroundColor) {
-//     this.style.backgroundColor = '';
-//   } else {
-//     this.style.backgroundColor = 'red';
-//   }
-// };
