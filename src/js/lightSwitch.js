a255d4ff571db1fb461765b817f84d208cc6cb9e
lmc-cmd <63010584+lmc-cmd@users.noreply.github.com>
2022-07-18 16:58:17 +0300

    colors UPD

--- src/js/lightSwitch.js
+++ src/js/lightSwitch.js
@@ -3,42 +3,21 @@ import ls from './storage';
 
 const cneckBoxEl = document.querySelector('[name="ligthswitcher"]');
 const bodyEl = document.querySelector('body');
-const footerEl = document.querySelector('footer');
-let tittleEls = document.getElementsByClassName('film-tittle');
-console.log(tittleEls);
+
 const switherMode = ls.load('light');
 if (switherMode === 'off') {
   cneckBoxEl.checked = true;
   bodyEl.classList.add('night');
-  footerEl.classList.add('night');
-
-  // for (let i = 0; i < tittleEls.length; i += 1) {
-  //   tittleEls[i] = tittleEls[i].classList.add('nigthText');
-  // }
-  // for (let el of tittleEls) {
-  //   // el = el.style.color = 'white';
-  //   el = el.classList.add('nigthText');
-  // }
 }
-console.log(tittleEls);
+
 cneckBoxEl.addEventListener('change', nigthlight);
 
 function nigthlight() {
   if (cneckBoxEl.checked) {
     ls.save('light', 'off');
     bodyEl.classList.add('night');
-    footerEl.classList.add('night');
-    for (let el of tittleEls) {
-      // el = el.style.color = 'white';
-      el = el.classList.add('nigthText');
-    }
   } else {
     bodyEl.classList.remove('night');
-    footerEl.classList.remove('night');
     ls.save('light', 'on');
-    for (let el of tittleEls) {
-      // el = el.style.color = 'black';
-      el = el.classList.remove('nigthText');
-    }
   }
 }
